5325e85960c97439faafa6646ee1f1d5aba146a7
Abbos Iskandarov <iskandarov.foto@gmail.com>
2020-12-11 15:48:36 +0500

    with JS

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+var burgerButton = document.querySelector('#burger_button');
+var overlay = document.querySelector('#overlay'); 
+var body = document.querySelector('body');
+var mobileMenu = document.querySelector('#mobile_menu');
+
+burgerButton.addEventListener('click', function(e){
+  e.preventDefault();
+
+  this.classList.toggle('crossed');
+  overlay.classList.toggle('visible');
+  body.classList.toggle('no_scroll');
+  mobileMenu.classList.toggle('opened');
+});
